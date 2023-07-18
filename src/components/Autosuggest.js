91039ec5d2703d87277b3b00c4888b57d9ab4f50
Remi1095 <109382772+Remi1095@users.noreply.github.com>
2023-07-18 14:11:27 -0500

    Update Autosuggest.js

--- src/components/Autosuggest.js
+++ src/components/Autosuggest.js
@@ -13,24 +13,19 @@ function Autosuggest({ elements, handleElements, suggestions, placeholder }) {
     if (elements.length > 0 && values.length > elements.length && filteredSuggestions.length > 0) {
       values[values.length - 2] = filteredSuggestions[0];
     }
-    console.log(values);
-    console.log(elements);
+    
     setHasEmptyRow(values[values.length - 1] === '');
 
     if (values[values.length - 1] !== '') {
       const filtered = suggestions.filter((suggestion) =>
         suggestion.toLowerCase().startsWith(values[values.length - 1].toLowerCase())
       );
-      console.log(filtered);
       setFilteredSuggestions(filtered);
     } else {
       setFilteredSuggestions([]);
     }
 
-
     handleElements(values.filter((value) => value !== ''));
-
-
   };
 
   function handleSuggestionClick(suggestion) {
@@ -41,23 +36,11 @@ function Autosuggest({ elements, handleElements, suggestions, placeholder }) {
     handleElements(values.filter((value) => value !== ''))
   };
 
-  const handleKeyEnter = (event) => {
-    if (event.key === "Enter") {
-      //event.preventDefault(); // Prevent the default Enter key behavior
-
-      // Your custom code here
-      console.log("Enter key was pressed!");
-      // Perform any additional actions or manipulate the textarea content as needed
-    }
-  };
-
-
   return (
     <div className="mx-auto" style={{ position: "relative", width: "90%" }}>
       <textarea
         value={elements.join('\n') + ((hasEmptyRow && elements.length > 0) ? '\n' : '')}
         onChange={handleChange}
-        onKeyDown={handleKeyEnter}
         placeholder={placeholder}
         rows={elements.length + ((hasEmptyRow) ? 1 : 0)}
         style={{ resize: 'none', width: "100%" }}
